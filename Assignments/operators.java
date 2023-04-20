edf007ba5bdc7a3d265f95b4bca2f7bbf6e80703
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2023-04-20 11:44:47 +0530

    Create operators.java

---
+++ Assignments/operators.java
@@ -0,0 +1,20 @@
+
+public class operators {
+    public static void main(String[] args) {
+        
+        int a=6;
+
+        int postinc=a++;
+        int preinc=++a;
+         int predec=--a;
+         int postdec=a--;
+
+         System.out.println("Post increment:"+ postinc);
+         System.out.println("Pre increment:"+ preinc);
+
+         System.out.println("Pre decrement:"+ predec);
+
+         System.out.println("Post decrement:"+ postdec);
+ }
+    
+}
